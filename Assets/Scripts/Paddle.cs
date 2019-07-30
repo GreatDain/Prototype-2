0457d369b6fdb67c1458be7e0c9e443e91c5fb2f
Duncan <duncan.smale@gmail.com>
2019-07-30 20:26:09 +0200

    Added basic ball mechanics Added textmeshpro Added ejector script Added paddle script

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] KeyCode _ActivateKey;
+
+    [SerializeField] float _MinAngle;
+    [SerializeField] float _MaxAngle;
+
+    Quaternion _ActivatedRotation;
+    Quaternion _IdleRotation;
+
+    [SerializeField] float RotationSpeed = 10f;
+
+    private void Awake()
+    {
+        _ActivatedRotation = new Quaternion();
+        _ActivatedRotation = Quaternion.Euler(0, 0, _MaxAngle);
+        _IdleRotation = new Quaternion();
+        _IdleRotation = Quaternion.Euler(0, 0, _MinAngle);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ActivatePaddle();
+    }
+
+    void ActivatePaddle()
+    {
+        Quaternion currentRotation = transform.rotation;
+        if (Input.GetKey(_ActivateKey))
+        {
+            transform.rotation = Quaternion.Slerp(currentRotation, _ActivatedRotation, Time.deltaTime * RotationSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(currentRotation, _IdleRotation, Time.deltaTime * RotationSpeed);
+        }
+    }
+}
